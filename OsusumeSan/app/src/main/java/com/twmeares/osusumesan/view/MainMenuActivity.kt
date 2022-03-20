537f39eadf737c32c57ac4ddb6240e7da544d59c
Taylor Meares <twmeares@users.noreply.github.com>
2022-03-20 11:41:42 -0700

    Create the main menu screen and start launching the app from there.

---
+++ OsusumeSan/app/src/main/java/com/twmeares/osusumesan/view/MainMenuActivity.kt
@@ -0,0 +1,56 @@
+package com.twmeares.osusumesan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.twmeares.osusumesan.MainActivity
+import com.twmeares.osusumesan.R
+import com.twmeares.osusumesan.databinding.ActivityMainMenuBinding
+import com.twmeares.osusumesan.models.DictionaryResult
+import com.twmeares.osusumesan.viewmodels.GlossDialog
+import com.twmeares.osusumesan.viewmodels.MainMenu
+
+class MainMenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainMenuBinding
+    private lateinit var mainMenu: MainMenu
+    private val TAG: String = "MainMenuActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mainMenu = MainMenu(this)
+        //TODO add one time only content here like loading db's
+        setupClickListeners(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //TODO eventually need to check for enable/disable the resume button
+    }
+
+    private fun setupClickListeners(view: View) {
+
+        binding.btnReadingList.setOnClickListener {
+            Log.i(TAG, "clicked btnReadingList")
+            mainMenu.StartReadingList()
+        }
+
+        binding.btnResumeReading.setOnClickListener {
+            Log.i(TAG, "clicked btnResumeReading")
+            mainMenu.StartResumeReading()
+        }
+
+        binding.btnInputText.setOnClickListener {
+            Log.i(TAG, "clicked btnInputText")
+            mainMenu.StartInputText()
+        }
+
+        binding.btnSettings.setOnClickListener {
+            Log.i(TAG, "clicked btnSettings")
+            mainMenu.StartSettings()
+        }
+
+    }
+}
---
+++ OsusumeSan/app/src/main/java/com/twmeares/osusumesan/viewmodels/MainMenu.kt
@@ -0,0 +1,53 @@
+package com.twmeares.osusumesan.viewmodels
+
+import android.content.Intent
+
+import android.app.Activity
+import android.util.Log
+import com.twmeares.osusumesan.MainActivity
+
+
+class MainMenu() {
+    private var activity: Activity? = null
+    private val TAG: String = "MainMenu"
+
+//    fun MainMenu(context: Activity?) {
+//        activity = context
+//    }
+
+    constructor(context: Activity?) : this() {
+        this.activity = context
+    }
+
+    fun StartReadingList() {
+        Log.i(TAG, "MainMenu StartReadingList")
+//        val intent = Intent(activity, ReadingListActivity class)
+//        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//        activity!!.startActivity(intent)
+    }
+
+    fun StartResumeReading() {
+        Log.i(TAG, "MainMenu StartResumeReading")
+        val intent = Intent(activity, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        // TODO probably need some putExtra to tell the activity which reading task to load.
+        // Might need to handle differently between switch between pages and going back to reading
+        // vs when the app was completely closed and then trying to go back to the last reading.
+        //intent.putExtra("jobOffer", true)
+        activity!!.startActivity(intent)
+    }
+
+    fun StartInputText() {
+        Log.i(TAG, "MainMenu StartInputText")
+//        val intent = Intent(activity, InputTextActivity class)
+//        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//        activity!!.startActivity(intent)
+    }
+
+    fun StartSettings() {
+        Log.i(TAG, "MainMenu StartSettings")
+//        val intent = Intent(activity, SettingsActivity class)
+//        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//        activity!!.startActivity(intent)
+    }
+}
